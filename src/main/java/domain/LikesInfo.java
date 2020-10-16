db7ed6d98df430f110b99f8df5d0ce69983438c5
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-10-16 15:26:51 +0300

    getters and setters added

--- src/main/java/domain/LikesInfo.java
+++ src/main/java/domain/LikesInfo.java
@@ -2,5 +2,22 @@ package domain;
 
 public class LikesInfo {
     private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isCanPost() {
+        return canPost;
+    }
+
+    public void setCanPost(boolean canPost) {
+        this.canPost = canPost;
+    }
+
     private boolean canPost;
 }
--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -2,6 +2,79 @@ package domain;
 
 public class Post {
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
+    }
+
+    public String getMainImageUrl() {
+        return mainImageUrl;
+    }
+
+    public void setMainImageUrl(String mainImageUrl) {
+        this.mainImageUrl = mainImageUrl;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public int getViewersCount() {
+        return viewersCount;
+    }
+
+    public void setViewersCount(int viewersCount) {
+        this.viewersCount = viewersCount;
+    }
+
     private String imageUrl;
     private String ownerId;
     private String data;
