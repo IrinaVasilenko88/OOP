6ef69da9cc75d35afe7d52b159831cc184a7fc7b
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-10-16 15:21:30 +0300

    Initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Post {
+    private int id;
+    private String imageUrl;
+    private String ownerId;
+    private String data;
+    private String postText;
+    private String mainImageUrl;
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private int viewersCount;
+
+}
